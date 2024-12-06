ad44c082d3e97fba8a02e2f8d2312d1d7c2995bc
albin <albin.bryn@gmail.com>
2024-12-06 15:54:20 +0100

    ändrar storlek på fönstret

--- src/System.cpp
+++ src/System.cpp
@@ -12,7 +12,7 @@ namespace cwing
 		
 		SDL_Init(SDL_INIT_EVERYTHING);
 		win = SDL_CreateWindow("Cwing", SDL_WINDOWPOS_CENTERED,
-			SDL_WINDOWPOS_CENTERED, 600, 400, 0);
+			SDL_WINDOWPOS_CENTERED, 1000, 1000, 0);
 		ren = SDL_CreateRenderer(win, -1, 0);
 		TTF_Init();
 		font = TTF_OpenFont( (constants::gResPath + "fonts/arial.ttf").c_str(), 36);
